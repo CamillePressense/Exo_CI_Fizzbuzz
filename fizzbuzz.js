e6b7c8fbd2d7e1bc91cf8394c2b7502645db699b
Pressense Fasani <camillefasani@gmail.com>
2025-07-15 11:40:19 +0200

    fix: function for input 15

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,13 @@
 const fizzbuzz = (number) => {
+  if ((number % 3 == 0) && (number % 5 == 0)){
+    return "Fizzbuzz";
+  }
+
   if (number % 3 == 0) {
-    return "Fizz"
+    return "Fizz";
   }
   if (number === 5) {
-    return "Buzz"
+    return "Buzz";
   }
   return number
 }
